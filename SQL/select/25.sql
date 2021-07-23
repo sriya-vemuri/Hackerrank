e5d615390d6463f17bdddd42b2f996c89bdad30c
Sriya Vemuri <84768384+sriya-vemuri@users.noreply.github.com>
2021-07-23 19:49:50 +0530

    Create 25.sql

---
+++ SQL/select/25.sql
@@ -0,0 +1,7 @@
+/*
+the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+*/
+
+select C.company_code,C.founder,count(distinct L.lead_manager_code),count(distinct S.senior_manager_code),count(distinct M.manager_code),count(distinct E.employee_code) from Company C, Lead_Manager L, Senior_Manager S, Manager M, Employee E 
+where C.company_code = L.company_code and L.lead_manager_code = S.lead_manager_code and S.senior_manager_code = M.senior_manager_code and M.manager_code = E.manager_code group by C.company_code,C.founder
+order by C.company_code;
